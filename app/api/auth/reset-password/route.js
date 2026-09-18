0bc85ad49985e2289752d46192240a0f6119a8f6
Abid Balouch <abidbalouch@gmail.com>
2026-09-18 17:32:36 +0500

    reset password page email fixing mail

--- app/api/auth/reset-password/route.js
+++ app/api/auth/reset-password/route.js
@@ -23,12 +23,7 @@ export async function POST(request) {
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          code,
-          email,
-          new_password: password,
-          AUTH_KEY: process.env.WP_JWT_AUTH_KEY,
-        }),
+        body: JSON.stringify({ code, email, new_password: password }),
       },
     );
 
